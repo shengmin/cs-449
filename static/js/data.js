a3863575de74a961080887bd11353d941d7e17d6
ShengMin Zhang <me@shengmin.me>
2013-08-04 03:33:16 -0400

    WIP

---
+++ static/js/data.js
@@ -0,0 +1,51 @@
+'use strict';
+
+var cs449 = {};
+
+cs449.tasks = [
+  {
+    category: 'new',
+    name: 'Task 1',
+    'for': 'ShengMin Zhang',
+    messages: [
+      {
+        iconClass: 'icon-phone',
+        actionClass: 'actionClass',
+        fromName: 'ShengMin Zhang',
+        fromId: 'me@shengmin.me',
+        date: 'July 19, 2013',
+        content: 'Hi Linda, I want to talk about blah blah blah blah'
+      },
+      {
+        iconClass: 'icon-phone',
+        actionClass: 'actionClass',
+        fromName: 'fromName',
+        date: 'July 19, 2013',
+        fromId: 'fromId',
+        content: 'content'
+      },
+      {
+        iconClass: 'icon-mail',
+        actionClass: 'actionClass',
+        fromName: 'fromName',
+        date: 'July 19, 2013',
+        fromId: 'fromId',
+        content: 'content'
+      }
+    ]
+  },
+  {
+    category: 'everything-else',
+    name: 'Task 2',
+    'for': 'ShengMin Zhang',
+    messages: [
+      {
+        iconClass: 'icon-phone',
+        actionClass: 'actionClass',
+        fromName: 'fromName',
+        fromId: 'fromId',
+        content: 'content'
+      }
+    ]
+  }
+];
