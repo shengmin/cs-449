2cc6ee834f2e7ba6c26205727db76849cb168d17
ShengMin Zhang <me@shengmin.me>
2013-08-04 11:06:20 -0400

    - Finish Task List View

--- static/js/data.js
+++ static/js/data.js
@@ -2,50 +2,94 @@
 
 var cs449 = {};
 
-cs449.tasks = [
-  {
-    category: 'new',
-    name: 'Task 1',
-    'for': 'ShengMin Zhang',
-    messages: [
-      {
-        iconClass: 'icon-phone',
-        actionClass: 'actionClass',
-        fromName: 'ShengMin Zhang',
-        fromId: 'me@shengmin.me',
-        date: 'July 19, 2013',
-        content: 'Hi Linda, I want to talk about blah blah blah blah'
-      },
-      {
-        iconClass: 'icon-phone',
-        actionClass: 'actionClass',
-        fromName: 'fromName',
-        date: 'July 19, 2013',
-        fromId: 'fromId',
-        content: 'content'
-      },
-      {
-        iconClass: 'icon-mail',
-        actionClass: 'actionClass',
-        fromName: 'fromName',
-        date: 'July 19, 2013',
-        fromId: 'fromId',
-        content: 'content'
-      }
-    ]
-  },
-  {
-    category: 'everything-else',
-    name: 'Task 2',
-    'for': 'ShengMin Zhang',
-    messages: [
-      {
-        iconClass: 'icon-phone',
-        actionClass: 'actionClass',
-        fromName: 'fromName',
-        fromId: 'fromId',
-        content: 'content'
-      }
-    ]
-  }
-];
+(function () {
+  var CAT_PRIORITY = '1 - Priority';
+  var CAT_WAITING = '2 - Waiting on Others';
+  var CAT_EVERYTHING_ELSE = '3 - Everything Else';
+
+  cs449.tasks = [
+    {
+      category: CAT_PRIORITY,
+      name: 'Task 1',
+      contact: 'ShengMin Zhang',
+      summary: 'summary1',
+      date: 'July 19, 2013',
+      messages: [
+        {
+          iconClass: 'icon-phone',
+          actionClass: 'actionClass',
+          fromName: 'ShengMin Zhang',
+          fromId: 'me@shengmin.me',
+          date: 'July 19, 2013',
+          content: 'Hi Linda, I want to talk about blah blah blah blah'
+        },
+        {
+          iconClass: 'icon-phone',
+          actionClass: 'actionClass',
+          fromName: 'fromName',
+          date: 'July 19, 2013',
+          fromId: 'fromId',
+          content: 'content'
+        },
+        {
+          iconClass: 'icon-mail',
+          actionClass: 'actionClass',
+          fromName: 'fromName',
+          date: 'July 19, 2013',
+          fromId: 'fromId',
+          content: 'content'
+        }
+      ]
+    },
+    {
+      category: CAT_WAITING,
+      name: 'Task 2',
+      contact: 'ShengMin Zhang',
+      summary: 'summary2',
+      date: 'July 19, 2013',
+      messages: [
+        {
+          iconClass: 'icon-phone',
+          actionClass: 'actionClass',
+          fromName: 'fromName',
+          fromId: 'fromId',
+          content: 'content'
+        }
+      ]
+    },
+    {
+      category: CAT_EVERYTHING_ELSE,
+      name: 'Task 2',
+      contact: 'ShengMin Zhang',
+      summary: 'summary2',
+      date: 'July 19, 2013',
+      messages: [
+        {
+          iconClass: 'icon-phone',
+          actionClass: 'actionClass',
+          fromName: 'fromName',
+          fromId: 'fromId',
+          content: 'content'
+        }
+      ]
+    },
+    {
+      category: CAT_PRIORITY,
+      name: 'Task 2',
+      contact: 'ShengMin Zhang',
+      summary: 'summary2',
+      date: 'July 19, 2013',
+      messages: [
+        {
+          iconClass: 'icon-phone',
+          actionClass: 'actionClass',
+          fromName: 'fromName',
+          fromId: 'fromId',
+          content: 'content'
+        }
+      ]
+    }
+  ];
+})();
+
+
--- static/js/task-list.js
+++ static/js/task-list.js
@@ -1,55 +1,32 @@
+var listViewTemplate = $('#list-view-template').html();
 
-  var localDataSource = new kendo.data.HierarchicalDataSource({
-    data: cs449.tasks,
-    schema: {
-        model: {
-            children: "messages"
-        }
-    }
-});
-
-var groupedData = [
-        {name: "Sashimi salad", letter: "S" },
-        {name: "Chirashi sushi", letter: "C" },
-        {name: "Seaweed salad", letter: "S" },
-        {name: "Edamame", letter: "E" },
-        {name: "Miso soup", letter: "M" },
-        {name: "Maguro", letter: "M" },
-        {name: "Shake", letter: "S" },
-        {name: "Shiromi", letter: "S" },
-        {name: "Tekka maki", letter: "T" },
-        {name: "Hosomaki Mix", letter: "H" },
-        {name: "California rolls", letter: "C" },
-        {name: "Seattle rolls", letter: "S" },
-        {name: "Spicy Tuna rolls", letter: "S" },
-        {name: "Ebi rolls", letter: "E" },
-        {name: "Chicken Teriyaki", letter: "C" },
-        {name: "Salmon Teriyaki", letter: "S" },
-        {name: "Gohan", letter: "G" },
-        {name: "Tori Katsu", letter: "T" },
-        {name: "Yaki Udon", letter: "Y" }
-    ];
-
-function onCategoryViewInit() {
-  $("#category-list").kendoMobileListView({
-            dataSource: localDataSource,
-            template: "${name}",
-            fixedHeaders: true
-        });
+function onContactViewInit() {
+    $('#contact-list').kendoMobileListView({
+        template: listViewTemplate,
+        dataSource: kendo.data.DataSource.create({
+            data: cs449.tasks,
+            group: { field: 'contact' }
+        })
+    })
 }
 
+function onCategoryViewInit() {
+    $('#category-list').kendoMobileListView({
+        template: listViewTemplate,
+        dataSource: kendo.data.DataSource.create({
+            data: cs449.tasks,
+            group: { field: 'category' }
+        })
+    });
+}
 
-function initForm() {
-    $("#dropdown").kendoDropDownList({
-         dataSource: [ { Id: 0, Title: "Manager" }, { Id: 1, Title: "Developer" }, { Id: 2, Title: "Vice President" } ],
-         animation: {
-            open: {
-                effects: "fadeIn",
-                duration: 300,
-                show: true
-            }
-         },
-         dataTextField: "Title",
-    dataValueField: "Id"
-     });
+function onGroupByPopOverInit(e) {
+    $('input[name=group-by-select]').click(function() {
+        window.location.hash = $(this).val();
+    });
+}
+
+function onGroupByPopOverClick(e) {
+    $('#group-by-popover').data('kendoMobilePopOver').close();
+
 }
