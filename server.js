b38cf730dd41befb88682ef5fcb1feb7a619f08d
ShengMin Zhang <me@shengmin.me>
2013-08-05 02:56:16 -0400

    add device wrapper

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 
 app.use(express.compress());
-app.use(express.static(__dirname + '/static'));
+app.use(express.static(__dirname + '/static'), { maxAge: 3600 * 24});
 
 app.get('/', function(reqeust, response) {
   response.redirect(301, '/task');
