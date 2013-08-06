48eb378399d89e8eb6f1c63bd49a53cad8de8361
ShengMin Zhang <me@shengmin.me>
2013-08-05 23:18:50 -0400

    remove duplicates views

--- static/js/mobile-application.js
+++ static/js/mobile-application.js
@@ -1,5 +1,51 @@
 'use strict';
 
+var DS_TASKS = kendo.data.DataSource.create({
+  data: cs449.tasks,
+  group: { field: 'category' }
+});
+
 var currentTask = null;
 var currentMessage = null;
 
+function onTaskListViewInit() {
+  $('#task-list').kendoMobileListView({
+    template: $('#task-template').html(),
+    fixedHeaders: true,
+    dataSource: DS_TASKS
+  });
+
+  $('#task-list input[type="checkbox"]').change(function() {
+    toggleHeader();
+  });
+}
+
+function onTaskListViewAfterShow() {
+  toggleHeader();
+}
+
+function toggleHeader() {
+  var size = $('#task-list-view #task-list input[type="checkbox"]:checked').size();
+  $('#task-menu').toggle(size > 0);
+  $('#unselected-navbar').toggle(size <= 0);
+}
+
+function onGroupByPopOverInit() {
+  $('input[name=group-by-select]').click(function() {
+    var groupBy = $(this).val();
+    if (groupBy) {
+      DS_TASKS.group(groupBy);
+    }
+    var taskList = $('#task-list').data('kendoMobileListView');
+    taskList.setDataSource(DS_TASKS);
+  });
+}
+
+function onRemindMePopOverInit() {
+  $('#remind-me-popover input[type="radio"]').click(function() {
+    $('#remind-me-popover').data('kendoMobilePopOver').close();
+    $(this).prop('checked', false);
+    // Remove the tasks that have been selected
+    $('#task-list-view #task-list input[type="checkbox"]:checked').prop('checked', false);
+  });
+}
--- static/js/task-list.js
+++ static/js/task-list.js
@@ -1,41 +1,3 @@
-var listViewTemplate = $('#list-view-template').html();
-
-function onContactViewInit() {
-    $('#contact-list').kendoMobileListView({
-        template: listViewTemplate,
-        fixedHeaders: true,
-        dataSource: kendo.data.DataSource.create({
-            data: cs449.tasks,
-            group: { field: 'contact' }
-        })
-    });
-
-    addCheckBoxChange('#contact-view');
-}
-
-function onCategoryViewInit() {
-    $('#category-list').kendoMobileListView({
-        template: listViewTemplate,
-        fixedHeaders: true,
-        dataSource: kendo.data.DataSource.create({
-            data: cs449.tasks,
-            group: { field: 'category' }
-        })
-    });
-
-    addCheckBoxChange('#category-view');
-}
-
-function onAllViewInit() {
-    $('#all-list').kendoMobileListView({
-        template: listViewTemplate,
-        dataSource: kendo.data.DataSource.create({
-            data: cs449.tasks,
-            sort: { field: 'timestamp', dir: 'desc' }
-        })
-    });
-    addCheckBoxChange('#all-view');
-}
 
 function addCheckBoxChange(view) {
     $(view + ' input[type="checkbox"]').change(function() {
@@ -43,31 +5,10 @@ function addCheckBoxChange(view) {
     });
 }
 
-function onViewAfterShow(e) {
-    refreshMenu(e.view.id);
-}
-
-function refreshMenu(view) {
-    var size = $(view + ' input[type="checkbox"]:checked').size();
-    $(view + ' #task-menu').toggle(size > 0);
-    $(view + ' #unselected-navbar').toggle(size <= 0);
-}
-
 function onListClick(e) {
     if ($(e.target).closest('.content-column').size()) {
         window.location = '/task/' + e.dataItem.id;
     }
-}
-
-function onGroupByPopOverInit(e) {
-    $('input[name=group-by-select]').click(function() {
-        window.location.hash = $(this).val();
-        $('#group-by-popover').data('kendoMobilePopOver').close();
-    });
-}
-
-function onLayoutShow(e) {
-    // refreshMenu(e.view.id);
 }
 
 function onContentColumnSwipe(e) {
